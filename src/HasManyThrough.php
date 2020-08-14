743967f3a2aaeaf03e95fdfda43f9c3b3eb4a936
aogg <aogg@users.noreply.github.com>
2020-08-14 10:09:16 +0800

    添加注释

--- src/HasManyThrough.php
+++ src/HasManyThrough.php
@@ -10,18 +10,20 @@ namespace aogg\think\relation;
 class HasManyThrough extends \think\model\relation\HasManyThrough
 {
     /**
+     * 关联关系获取第一个model的数据
+     * 
      * 执行基础查询（仅执行一次）
      * @access protected
      * @return void
      */
-    protected function baseQuery(): void
+    protected function baseQuery($dataBool = false): void
     {
-        if (empty($this->baseQuery) && $this->parent->getData()) {
-            $alias        = (new $this->model)->getTable();
+        if (empty($this->baseQuery) && ($this->parent->getData() || $dataBool)) {
+            $alias        = (new $this->model)->getTable(); // 第一个model的表名，主表
             $throughTable = $this->through->getTable();
             $pk           = $this->throughPk;
             $throughKey   = $this->throughKey;
-            $modelTable   = $this->parent->getTable();
+            $modelTable   = $this->parent->getTable(); // 当前model表面
             $fields       = $this->getQueryFields($alias);
 
             $this->query
