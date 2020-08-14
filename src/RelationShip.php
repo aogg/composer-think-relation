890ef185dfc5ae85ac4a0d75a4c718cefa25994c
aogg <aogg@users.noreply.github.com>
2020-08-14 18:20:50 +0800

    Update RelationShip.php

--- src/RelationShip.php
+++ src/RelationShip.php
@@ -9,8 +9,8 @@ trait RelationShip {
     /**
      * HAS MANY 远程关联定义
      * @access public
-     * @param  string $model      需要的模型名
-     * @param  string $through    中间模型名
+     * @param  string $model      需要的模型名    主表模型，返回的数据用此model转载
+     * @param  string $through    中间模型名      关联模型
      * @param  string $foreignKey 中间模型名 的 关联外键
      * @param  string $throughKey 需要的模型名 的 关联外键
      * @param  string $localKey   当前模型 的 主键
@@ -28,5 +28,26 @@ trait RelationShip {
         $throughPk  = $throughPk ?: (new $through)->getPk();
 
         return new HasManyThrough($this, $model, $through, $foreignKey, $throughKey, $localKey, $throughPk);
+    }
+
+    /**
+     * HAS MANY 远程关联定义
+     * 支持join的on条件
+     *
+     * @param string $model
+     * @param string $throughModel
+     * @param $modelOn
+     * @param $throughOn
+     * @param string $localKey
+     * @return HasManyThrough
+     */
+    public function hasManyThroughOnFncLocal(string $model, string $throughModel, $modelOn, $throughOn, string $localKey = '')
+    {
+        $localKey   = $localKey ?: $this->getPk();
+
+        return (new HasManyThrough($this, $model, $throughModel, '', '', $localKey, ''))
+            ->setModelOn($modelOn)
+            ->setThroughOn($throughOn)
+            ;
     }
 }
